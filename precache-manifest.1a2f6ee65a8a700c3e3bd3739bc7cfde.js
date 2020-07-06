b9fe25753395080292cfacc60ebd1e4272958d75
Hudson Daniel <hudsonprem97@gmail.com>
2020-07-06 20:22:14 +0530

    Updates

---
+++ precache-manifest.1a2f6ee65a8a700c3e3bd3739bc7cfde.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "448efd78a9457c2b368a8ffb090b9a2e",
+    "url": "/Hudsonprem/pokecards/index.html"
+  },
+  {
+    "revision": "c562bca94c5b687a71f7",
+    "url": "/Hudsonprem/pokecards/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "0b76445cc99fe4b6cdd4",
+    "url": "/Hudsonprem/pokecards/static/css/main.c5aa055b.chunk.css"
+  },
+  {
+    "revision": "c562bca94c5b687a71f7",
+    "url": "/Hudsonprem/pokecards/static/js/2.40d9d520.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Hudsonprem/pokecards/static/js/2.40d9d520.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b76445cc99fe4b6cdd4",
+    "url": "/Hudsonprem/pokecards/static/js/main.618d0358.chunk.js"
+  },
+  {
+    "revision": "c86aaf320d1edfd828c8",
+    "url": "/Hudsonprem/pokecards/static/js/runtime-main.3371967e.js"
+  },
+  {
+    "revision": "c5e7d25b3bb0f11d8689530e2e4c5d14",
+    "url": "/Hudsonprem/pokecards/static/media/EridanusCondensed3DItalic-1gXL.c5e7d25b.otf"
+  },
+  {
+    "revision": "a50e9724220f5182022964fc97251ead",
+    "url": "/Hudsonprem/pokecards/static/media/OverstreetBible-Mnmw.a50e9724.otf"
+  }
+]);
